ca40d6d198171322e47c907c222f961cbef8cd1e
deadinsky <deadinsky66@gmail.com>
2017-11-20 22:56:30 -0500

    Made the database fill based on a json file

--- app/src/main/java/com/thomas/dedinsky/DataStructuresApp/MainActivity.java
+++ app/src/main/java/com/thomas/dedinsky/DataStructuresApp/MainActivity.java
@@ -2,15 +2,28 @@ package com.thomas.dedinsky.DataStructuresApp;
 
 import android.app.Application;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetManager;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+import org.json.*;
 
 import com.thomas.dedinsky.DataStructuresApp.databinding.ActivityMainBinding;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Iterator;
 
 class MyAppApplication extends Application {
     private String chapterName;
@@ -26,23 +39,72 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        saveToDB();
         MyAppApplication mApp = ((MyAppApplication)getApplicationContext());
         mApp.setChapterName("default");
         mApp.setLessonID(-1);
+        checkData();
         startActivity(new Intent(MainActivity.this, com.thomas.dedinsky.DataStructuresApp.OverviewActivity.class));
     }
 
-    private void saveToDB() {
-        SQLiteDatabase database = new SampleDBSQLiteHelper(this).getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.put(SampleDBContract.Lesson.COLUMN_NAME, "jim");
-        values.put(SampleDBContract.Lesson.COLUMN_CHAPTER, "jim");
-        values.put(SampleDBContract.Lesson.COLUMN_SUMMARY, "jim");
-        values.put(SampleDBContract.Lesson.COLUMN_CODE, "jim");
+    protected void checkData() {
+        MyAppApplication mApp = ((MyAppApplication)getApplicationContext());
+        SQLiteDatabase database = new SampleDBSQLiteHelper(this).getReadableDatabase();
+        String[] projection = {
+                SampleDBContract.Lesson._ID,
+                SampleDBContract.Lesson.COLUMN_NAME,
+                SampleDBContract.Lesson.COLUMN_CHAPTER,
+                SampleDBContract.Lesson.COLUMN_SUMMARY,
+                SampleDBContract.Lesson.COLUMN_CODE
+        };
+        String selection =
+                SampleDBContract.Lesson._ID + " = ?";
+        String[] selectionArgs = {Integer.toString(mApp.getLessonID())};
+        Cursor cursor = database.rawQuery("SELECT count(*) FROM "+SampleDBContract.Lesson.TABLE_NAME, null);
+        cursor.moveToFirst();
+        int count = cursor.getInt(0);
+        Log.d("meme", Integer.toString(count));
+        if (count == 0) {
+            try {
+                readJSON();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-        long newRowId = database.insert(SampleDBContract.Lesson.TABLE_NAME, null, values);
-
-        Toast.makeText(this, "The new Row Id is " + newRowId, Toast.LENGTH_LONG).show();
+    protected void readJSON() throws IOException, JSONException {
+        AssetManager assetManager = getApplicationContext().getAssets();
+        InputStream inputStream = assetManager.open("DataStructuresTest.txt");
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader reader = new BufferedReader(inputStreamReader);
+        String json = "";
+        StringBuilder sb = new StringBuilder();
+        String line = reader.readLine();
+        while (line != null) {
+            sb.append(line);
+            sb.append("\n");
+            line = reader.readLine();
+        }
+        json = sb.toString();
+        reader.close();
+        Log.d("JSON", json);
+        if (json != "") {
+            SQLiteDatabase database = new SampleDBSQLiteHelper(this).getWritableDatabase();
+            JSONObject obj = new JSONObject(json);
+            JSONArray array= obj.getJSONArray("data");
+            for(int i = 0; i < array.length(); i++){
+                ContentValues values = new ContentValues();
+                JSONObject row = array.getJSONObject(i);
+                Log.d("JSON", row.getString("name"));
+                values.put(SampleDBContract.Lesson.COLUMN_NAME, row.getString("name"));
+                values.put(SampleDBContract.Lesson.COLUMN_CHAPTER, row.getString("chapter"));
+                values.put(SampleDBContract.Lesson.COLUMN_SUMMARY, row.getString("summary"));
+                values.put(SampleDBContract.Lesson.COLUMN_CODE, row.getString("code"));
+                database.insert(SampleDBContract.Lesson.TABLE_NAME, null, values);
+                //Toast.makeText(this, "The new Row Id is " + newRowId, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
--- app/src/main/java/com/thomas/dedinsky/DataStructuresApp/SampleDBSQLiteHelper.java
+++ app/src/main/java/com/thomas/dedinsky/DataStructuresApp/SampleDBSQLiteHelper.java
@@ -3,7 +3,6 @@ package com.thomas.dedinsky.DataStructuresApp;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.util.Log;
 
 public class SampleDBSQLiteHelper extends SQLiteOpenHelper {
 
