0bd9ddc8da4f9da3504ef1b4959b2a5a6089ad0e
deadinsky <deadinsky66@gmail.com>
2017-12-05 15:46:21 -0500

    Added support for portrait orientation

--- app/src/main/java/com/thomas/dedinsky/DataStructuresApp/LessonActivity.java
+++ app/src/main/java/com/thomas/dedinsky/DataStructuresApp/LessonActivity.java
@@ -1,6 +1,8 @@
 package com.thomas.dedinsky.DataStructuresApp;
 
 import android.content.ContentValues;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.databinding.DataBindingUtil;
@@ -23,39 +25,77 @@ import java.util.Calendar;
 
 public class LessonActivity extends AppCompatActivity {
 
+    private LinearLayout content1;
+    private LinearLayout contentA;
     private LinearLayout section1;
     private LinearLayout section2;
+    private LinearLayout sectionA;
     private TextView text1;
     private TextView text2;
+    private TextView textA;
     private static final String TAG = "LessonActivity";
     protected String name;
     protected String summary;
     protected String code;
+    protected int orientation;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            LinearLayout.LayoutParams lp = (LinearLayout.LayoutParams) section2.getLayoutParams();
+            LinearLayout.LayoutParams lp = (LinearLayout.LayoutParams) contentA.getLayoutParams();
+            LinearLayout.LayoutParams lp2 = (LinearLayout.LayoutParams) section2.getLayoutParams();
+            LinearLayout.LayoutParams lpA = (LinearLayout.LayoutParams) sectionA.getLayoutParams();
+            if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                textA.setText("");
+                lpA.weight = 0;
+                section2.setLayoutParams(lpA);
+                lp.weight = 0;
+                contentA.setLayoutParams(lp);
+            } else {
+                text2.setText("");
+                lp2.weight = 0;
+                section2.setLayoutParams(lp2);
+                lp.weight = 1;
+                contentA.setLayoutParams(lp);
+            }
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     text1.setText(summary);
-                    text2.setText("");
-                    lp.weight = 0;
-                    section2.setLayoutParams(lp);
+                    if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                        text2.setText("");
+                        lp2.weight = 0;
+                        section2.setLayoutParams(lp2);
+                    } else {
+                        textA.setText("");
+                        lpA.weight = 0;
+                        section2.setLayoutParams(lpA);
+                    }
                     return true;
                 case R.id.navigation_dashboard:
                     text1.setText(summary);
-                    text2.setText(code);
-                    lp.weight = 1;
-                    section2.setLayoutParams(lp);
+                    if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                        text2.setText(code);
+                        lp2.weight = 1;
+                        section2.setLayoutParams(lp2);
+                    } else {
+                        textA.setText(code);
+                        lpA.weight = 1;
+                        section2.setLayoutParams(lpA);
+                    }
                     return true;
                 case R.id.navigation_notifications:
                     text1.setText(code);
-                    text2.setText("");
-                    lp.weight = 0;
-                    section2.setLayoutParams(lp);
+                    if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                        text2.setText("");
+                        lp2.weight = 0;
+                        section2.setLayoutParams(lp2);
+                    } else {
+                        textA.setText("");
+                        lpA.weight = 0;
+                        section2.setLayoutParams(lpA);
+                    }
                     return true;
             }
             return false;
@@ -68,15 +108,18 @@ public class LessonActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson);
         readFromDB();
+        orientation = getResources().getConfiguration().orientation;
+        content1 = (LinearLayout) findViewById(R.id.content1);
+        contentA = (LinearLayout) findViewById(R.id.contentA);
         section1 = (LinearLayout) findViewById(R.id.section1);
         section2 = (LinearLayout) findViewById(R.id.section2);
+        sectionA = (LinearLayout) findViewById(R.id.sectionA);
         text1 = (TextView) findViewById(R.id.text1);
-        text1.setText(summary);
         text2 = (TextView) findViewById(R.id.text2);
+        textA = (TextView) findViewById(R.id.textA);
+        text1.setText(summary);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-        //text1.setText(summary);
     }
 
     private void readFromDB() {
