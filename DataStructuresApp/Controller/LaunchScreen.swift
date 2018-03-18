169564fceb448c67ee9974eb47532178a92e791c
Ethan Chan <ethan.chan_@live.ca>
2018-03-18 17:20:03 -0400

    added load screen

---
+++ DataStructuresApp/Controller/LaunchScreen.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchScreen.swift
+//  DataStructuresApp
+//
+//  Created by Ethan Chan on 2018-03-18.
+//  Copyright © 2018 Waddah Aldrobi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LaunchScreen: UIViewController{
+    let gradientLayer = CAGradientLayer()
+    override func viewDidLoad() {
+        gradientLayer.frame = self.view.bounds
+    }
+}
