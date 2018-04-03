7a87866183340352bf75bf1139eb151502b57743
Waddah Aldrobi <aldrobiwaddah@gmail.com>
2018-04-03 05:26:15 -0400

    Implemented the 'no output' on first tap thing for codeVC

--- DataStructuresApp/Controller/CodeVC.swift
+++ DataStructuresApp/Controller/CodeVC.swift
@@ -9,8 +9,12 @@ class CodeVC: UIViewController{
     var cellRun : [Any] = []
     var cellDesc : [Any] = []
     var cellCount = 0
+    
+    // Control what is displayed in the taps
     var tap1 = Array(repeating: false, count: 1)
     var tap2 = Array(repeating: false, count: 1)
+    // Checks if the cell has a "run" output
+    var NoOutput = [Bool]()
     
     var highlightr = Highlightr()
     let textStorage = CodeAttributedString()
@@ -20,8 +24,10 @@ class CodeVC: UIViewController{
     @IBOutlet weak var tableViewCode: UITableView!
     
     override func viewDidLoad() {
+        // Automatically sets the height of tableview cells
         tableViewCode.estimatedRowHeight = tableViewCode.rowHeight
         tableViewCode.estimatedRowHeight = UITableViewAutomaticDimension
+        
         let subLessonData = DataSingleton.shared.grabSpecific(tab: "Code") as! [String : Any]
         //Parse of JSON
         print("in code vc")
@@ -29,10 +35,20 @@ class CodeVC: UIViewController{
         cellRun = subLessonData["CellRun"] as! [Any]
         cellDesc = subLessonData["CellDesc"] as! [Any]
         
-        ////////////// Must change the data.count !!!!!!!
+        // Sets them all to false, nothing has to be displayed in the beginning
         cellCount = cellCode.count
         tap1 = Array(repeating: false, count: cellCount)
         tap2 = Array(repeating: false, count: cellCount)
+        
+        // Checks what does and doesn't have an output
+        for str in cellRun{
+            if String(describing: str) == "No Output"{
+                NoOutput.append(true)
+            }
+            else{
+                NoOutput.append(false)
+            }
+        }
         
         //Syntax Highlighting
         textStorage.highlightr.setTheme(to: "solarized-dark")
@@ -40,28 +56,11 @@ class CodeVC: UIViewController{
     }
     
     
-    // Controls orientation
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
-        //AppUtility.lockOrientation(.portrait)
-        // Or to rotate and lock
-//        AppUtility.lockOrientation(.portrait , andRotateTo: .portraitUpsideDown)
-//        AppUtility.lockOrientation(.portrait , andRotateTo: .portrait)
-        
-        //Sets title of tab
-         self.tabBarController?.navigationItem.title = "Code";
+        self.tabBarController?.navigationItem.title = "Code";
     }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        // Don't forget to reset when view is being removed
-//        AppUtility.lockOrientation(.portrait)
-    }
-    
 }
-
 
 
 
@@ -71,15 +70,25 @@ extension CodeVC : UITableViewDelegate{
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         myIndex = indexPath.row
         
-        if (tap1[myIndex] == false && tap2[myIndex] == false) {tap1[myIndex]=true}
-            
+        // First tap with an output
+        if (tap1[myIndex] == false && tap2[myIndex] == false && NoOutput[myIndex] == false) {tap1[myIndex]=true}
+        
+        // First tap, no output => Jumps to second tap
+        else if(tap1[myIndex] == false && tap2[myIndex] == false && NoOutput[myIndex] == true){
+            tap1[myIndex]=false
+            tap2[myIndex]=true
+        }
+        
+        // Second tap, with output
         else if (tap1[myIndex] == true && tap2[myIndex] == false) {
             tap1[myIndex]=false
             tap2[myIndex]=true
         }
-            
+        
+        // Third tap, goes to intital state
         else if (tap1[myIndex] == false && tap2[myIndex] == true) {tap2[myIndex]=false}
      
+        // Important to have the cells resize themselves
         tableView.reloadRows(at: [indexPath], with: UITableViewRowAnimation.fade)
     }
 }
@@ -97,22 +106,33 @@ extension CodeVC : UITableViewDataSource{
         
         cell.code.text = cellCode[indexPath.row] as! String
         if (tap1[myIndex] == false && tap2[myIndex] == false){cell.tapLabel.text = nil
+            // Hides the cells below them
             cell.tapLabelBottomConstraint.constant = -33
-            cell.tapLabel.backgroundColor = UIColor(red: 13/255, green: 42/255, blue: 53/255, alpha: 1)
-            cell.tapLabel.textColor = UIColor(red: 119/255, green: 135/255, blue: 138/255, alpha: 1)
-            cell.tapLabel.font = UIFont(name: "American Typewriter", size: 14)
         }
+        
         else if (tap1[myIndex] == true && tap2[myIndex] == false){
             cell.tapLabel.text = cellRun[indexPath.row] as! String
+            
+            // Shows output
             cell.tapLabelBottomConstraint.constant = 0
+            
+            // Just UI
             cell.tapLabel.backgroundColor = UIColor.black
             cell.tapLabel.textColor = UIColor.white
             cell.tapLabel.font = UIFont(name: "Avenir Book", size: 14)
             
         }
+        
         else if (tap1[myIndex] == false && tap2[myIndex] == true){
             cell.tapLabel.text = cellDesc[indexPath.row] as! String
+            
+            // Shows description
             cell.tapLabelBottomConstraint.constant = 0
+            
+            // Just UI
+            cell.tapLabel.backgroundColor = UIColor(red: 13/255, green: 42/255, blue: 53/255, alpha: 1)
+            cell.tapLabel.textColor = UIColor(red: 119/255, green: 135/255, blue: 138/255, alpha: 1)
+            cell.tapLabel.font = UIFont(name: "American Typewriter", size: 14)
         }
         
         //Highlighting
