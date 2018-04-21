225d29aeb47123ee1191d97f396885ac6b485cdf
Ethan Chan <ethan.chan_@live.ca>
2018-04-21 19:54:34 -0400

    fixed scrollview rotation issues

--- DataStructuresApp/Controller/VisualizationVC.swift
+++ DataStructuresApp/Controller/VisualizationVC.swift
@@ -14,8 +14,9 @@ class VisualizationVC: UIViewController, UIScrollViewDelegate {
     override func viewDidLoad() {
         let subLessonData = DataSingleton.shared.grabSpecific(tab: "Visualization") as! [String: Any]
         images = subLessonData["Images"] as! [String]
-
         //scrollview
+        var imageWidth = view.frame.width
+        var imageHeight = view.frame.height
         pageControl.numberOfPages = 0
         scrollView.delegate = self
         for image in images {
@@ -25,26 +26,28 @@ class VisualizationVC: UIViewController, UIScrollViewDelegate {
             contentWidth += view.frame.width
             scrollView.addSubview(imageView)
             if (((imageToDisplay?.size.height)! / (imageToDisplay?.size.width)!)*view.frame.width) < view.frame.height{ // decide whether to scale to height or width of screen
-                imageView.frame = CGRect(x: xCoordinate - (view.frame.width/2), y: (scrollView.frame.height / 2) - (view.frame.width/2), width: view.frame.width , height: ((imageToDisplay?.size.height)! / (imageToDisplay?.size.width)!)*view.frame.width)
+                imageWidth = view.frame.width
+                imageHeight = ((imageToDisplay?.size.height)! / (imageToDisplay?.size.width)!)*view.frame.width
             } else {
-                imageView.frame = CGRect(x: xCoordinate - (view.frame.width/2), y: (scrollView.frame.height / 2) - (view.frame.width/2), width: ((imageToDisplay?.size.width)! / (imageToDisplay?.size.height)!)*view.frame.height, height: view.frame.height)
+                imageWidth = ((imageToDisplay?.size.width)! / (imageToDisplay?.size.height)!)*view.frame.height
+                imageHeight = view.frame.height
             }
+            imageView.frame = CGRect(x: xCoordinate - (imageWidth/2), y: (view.frame.height/2) - (imageHeight/2), width: imageWidth, height: imageHeight)
             pageControl.numberOfPages += 1
         }
         
+        
         scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.height-150)
         
+        
     }
-    
-    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-        if UIDevice.current.orientation.isLandscape {
-            print("Landscape")
-            
-        } else if UIDevice.current.orientation.isPortrait {
-            print("Portrait")
-            
+    override func didRotate(from fromInterfaceOrientation: UIInterfaceOrientation) {
+        for subUIView in scrollView.subviews as [UIView] {
+            subUIView.removeFromSuperview()
         }
+        viewDidLoad()
     }
+
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         let width = view.frame.width
         pageControl.currentPage = Int(scrollView.contentOffset.x / CGFloat(width))
